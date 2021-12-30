bcb97a3818b4dcbd6fa6c2797b60bd4d11b26d60
mmasaaud <93669379+mmasaaud@users.noreply.github.com>
2021-12-30 10:55:55 -0600

    modify variables

--- variables.sh
+++ variables.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-CLUSTER_NAME='priv-fra-ssm-eks-cluster-01'
-REGION=eu-central-1
+CLUSTER_NAME='priv-eks-us-west-1'
+REGION=us-west-2
 HTTP_PROXY_ENDPOINT_SERVICE_NAME="" # leave blank for no proxy, or populate with a VPC endpoint ID to create a PrivateLink powered connection to a proxy server
 KEY_PAIR=""
-VERSION='1.16' # K8s version to deploy
-AMI_ID=ami-0bf7306240d09dcdd # AWS managed AMI for EKS worker nodes
+VERSION='1.20' # K8s version to deploy
+AMI_ID=ami-04f0700d5f614a923 # AWS managed AMI for EKS worker nodes
 INSTANCE_TYPE=t3.large # instance type for EKS worker nodes
-S3_STAGING_LOCATION=jasbarto-eks-frankfurt-cfn # S3 location to be created to store Cloudformation templates and a copy of the kubectl binary
+S3_STAGING_LOCATION=poc-octank-ab3-mmasaaud # S3 location to be created to store Cloudformation templates and a copy of the kubectl binary
 ENABLE_PUBLIC_ACCESS=false
 ENABLE_FARGATE=false
 FARGATE_PROFILE_NAME=PrivateFargateProfile
